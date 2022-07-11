a90b770654125bee8eab9a5cdf002dcac03fd6fa
katsup07 <90941888+katsup07@users.noreply.github.com>
2022-07-11 22:11:49 +0900

    Create Main.java

---
+++ Window-TemplateDesignPattern/Main.java
@@ -0,0 +1,15 @@
+package com.katsup.template;
+
+
+import com.codewithmosh1.template.TypeAWindow;
+import com.codewithmosh1.template.TypeBWindow;
+import com.codewithmosh1.template.Window;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Window window = new TypeAWindow();
+        window.close();
+
+        }
+    }
