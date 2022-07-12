59754e7b33cae81bde9e2723b55e7eb70e520130
katsup07 <90941888+katsup07@users.noreply.github.com>
2022-07-12 19:54:02 +0900

    Create Subject.java

---
+++ Stock-ObserverPattern/Subject.java
@@ -0,0 +1,24 @@
+package com.katsup.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject {
+    private List<Observer> observers = new ArrayList<>();
+
+    public void addObserver(Observer observer){
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer){
+        observers.remove(observer);
+    }
+
+    public void notifyObservers(){
+        for(var observer : observers){
+           // System.out.println("Went through subject!");
+            observer.update();
+        }
+    }
+
+}
